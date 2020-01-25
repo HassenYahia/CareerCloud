20341359c8644c5e490ba97deff8804c3ec70145
HassenYahia <hassenabdurahman4047@gmail.com>
2020-01-24 22:07:32 -0500

    initial commit

---
+++ CareerCloud.BusinessLogicLayer/ApplicantEducationLogic.cs
@@ -0,0 +1,55 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+   public class ApplicantEducationLogic : BaseLogic<ApplicantEducationPoco>
+    {
+        public ApplicantEducationLogic(IDataRepository<ApplicantEducationPoco> repository) : base(repository)
+        {
+        }
+        protected override void Verify(ApplicantEducationPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+
+                foreach (ApplicantEducationPoco poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.Major))
+                { 
+                    exceptions.Add(new ValidationException(107, $"Cannot be empty"));
+                }
+                else if (poco.Major.Length < 3)
+                { 
+                         exceptions.Add(new ValidationException(107, $"Cannot be Less than 3 Charactors"));
+                }
+                if (poco.StartDate > DateTime.Now)
+                { 
+                    exceptions.Add(new ValidationException(108, $"Cannot be greater than today"));
+                }
+                if (poco.CompletionDate < poco.StartDate )
+                { 
+                    exceptions.Add(new ValidationException(109, $"Cannot be earlier than Start date"));
+                }
+
+            }
+                if(exceptions.Count > 0)
+                  {
+                      throw new AggregateException(exceptions);
+                  }
+        }
+        public override void Add(ApplicantEducationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantEducationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+    }
+}
---
+++ CareerCloud.BusinessLogicLayer/SystemLanguageCodeLogic.cs
@@ -0,0 +1,73 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class SystemLanguageCodeLogic 
+    {
+        
+        protected IDataRepository<SystemLanguageCodePoco> _repository;
+        public SystemLanguageCodeLogic(IDataRepository<SystemLanguageCodePoco> repository)
+        {
+            _repository = repository;
+        }
+        protected void Verify(SystemLanguageCodePoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+
+            foreach (SystemLanguageCodePoco poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.LanguageID))
+                {
+                    exceptions.Add(new ValidationException(1000, $"Cannot be Empty"));
+                }
+                if (string.IsNullOrEmpty(poco.Name))
+                {
+                    exceptions.Add(new ValidationException(1001, $"Cannot be Empty"));
+                }
+                if (string.IsNullOrEmpty(poco.NativeName))
+                {
+                    exceptions.Add(new ValidationException(1002, $"Cannot be Empty"));
+                }
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+        
+
+        public virtual List<SystemLanguageCodePoco> GetAll()
+        {
+            return _repository.GetAll().ToList();
+        }
+
+        public virtual SystemLanguageCodePoco Get(string code)
+        {
+            return _repository.GetSingle(c => c.LanguageID  == code);
+        }
+        public virtual void Add(SystemLanguageCodePoco[] pocos)
+        {
+            Verify(pocos);
+            _repository.Add(pocos);
+        }
+
+        public virtual void Update(SystemLanguageCodePoco[] pocos)
+        {
+            Verify(pocos);
+            _repository.Update(pocos);
+        }
+
+        public void Delete(SystemLanguageCodePoco[] pocos)
+        {
+            _repository.Remove(pocos);
+        }
+    }
+}
+
+
